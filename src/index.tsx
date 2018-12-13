a4369f6a006f3abccdb2ca0e2a0153862b4c499f
Valiantsin <valiantsin.haradkou@icloud.com>
2018-12-13 15:51:25 +0300

    build: update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createMuiTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+
 import { App } from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const theme = createMuiTheme();
+
 ReactDOM.render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
