30279d41d91017c150f400f648390b2bb06a6478
Valiantsin <valiantsin.haradkou@icloud.com>
2019-01-09 16:50:37 +0300

    refactor: remove snackbar store, use hook instead

--- src/hooks/snackbar.hooks.tsx
+++ src/hooks/snackbar.hooks.tsx
@@ -1,30 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import Button from '@material-ui/core/Button';
 
-import { IObservableArray, observe, reaction } from 'mobx';
+import { reaction } from 'mobx';
 import { useDisposable } from 'mobx-react-lite';
-import { AsyncStatus, Snackbar } from 'models';
+import { AsyncStatus } from 'models';
 import { OptionsObject } from 'notistack';
 import { useStore } from 'stores';
 
 const DEFAULT_DURATION = 3000;
 
-function generateKey(): number {
-  return new Date().getTime() + Math.random();
-}
-
 // tslint:disable:jsx-no-lambda
 
 export function useSnackbar(enqueueSnackbar: (message: string, options?: OptionsObject) => void): void {
-  const { notesStore, snackbarStore } = useStore();
+  const { notesStore } = useStore();
 
   useDisposable(() => reaction(() => notesStore.addNoteStatus, (addNoteStatus) => {
     if (addNoteStatus === AsyncStatus.Success) {
-      const key = generateKey();
-      const message = 'Success add note';
-
-      snackbarStore.createSnackbar(key, message, {
+      enqueueSnackbar('Success add note', {
         variant: 'success',
         autoHideDuration: DEFAULT_DURATION,
         action: <Button onClick={() => notesStore.removeNote('')}>Undo</Button>,
@@ -34,24 +27,12 @@ export function useSnackbar(enqueueSnackbar: (message: string, options?: Options
     }
 
     if (addNoteStatus === AsyncStatus.Error) {
-      const key = generateKey();
-      const message = 'Error add note';
-
-      snackbarStore.createSnackbar(key, message, {
+      enqueueSnackbar('Error add note', {
         variant: 'error',
         autoHideDuration: DEFAULT_DURATION
       });
 
       notesStore.resetAddNoteStatus();
-    }
-  }));
-
-  useEffect(() => observe(snackbarStore.snackbars as IObservableArray<Snackbar>, (change) => {
-    if (change.type === 'splice' && change.addedCount > 0) {
-      change.added.forEach(addedItem => {
-        enqueueSnackbar(addedItem.message, addedItem.options);
-        snackbarStore.removeSnackbar(addedItem.key);
-      });
     }
   }));
 }
--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -1,11 +1,9 @@
 import { createContext, useContext } from 'react';
 
 import { NotesStore } from './notes.store';
-import { SnackbarStore } from './snackbar.store';
 
 const stores = Object.freeze({
   notesStore: new NotesStore(),
-  snackbarStore: new SnackbarStore(),
 });
 
 const StoreContext = createContext(stores);
